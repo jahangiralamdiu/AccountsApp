0faec53328f843d3c09978714af330f2506daee0
jahangiralamdiu <jahangiralamdiu@gmail.com>
2014-07-14 15:00:51 +0600

    Account calculation

---
+++ account.php
@@ -0,0 +1,70 @@
+<?php
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ * Description of account
+ *
+ * @author bcc
+ */
+class Account {
+    
+    private $account_name;
+    
+    private $account_number;
+    
+    private $balance;
+    
+    function __construct($account_name, $account_number) {
+        $this->account_name = $account_name;
+        $this->account_number = $account_number; 
+        $this->balance = 0;
+    }
+    
+    public function get_account_name() {
+        return $this->account_name;
+    }
+
+    public function get_account_number() {
+        return $this->account_number;
+    }
+
+    public function get_balance() {
+        return $this->balance;
+    }
+
+    public function set_account_name($account_name) {
+        $this->account_name = $account_name;
+    }
+
+    public function set_account_number($account_number) {
+        $this->account_number = $account_number;
+    }
+
+    public function set_balance($balance) {
+        $this->balance = $balance;
+    }
+    
+    public function deposit ($amount)
+    {
+        $this->balance = $this->balance + $amount;
+        
+        return $this->balance;
+    }
+    
+    public function withdraw ($amount)
+    {
+        if($this->balance - $amount >0 )
+       {
+            
+       $this->balance = $this->balance - $amount; 
+             
+        }
+        return $this->balance;
+    }
+
+}
---
+++ index.php
@@ -0,0 +1,67 @@
+<!DOCTYPE html>
+<!--
+To change this license header, choose License Headers in Project Properties.
+To change this template file, choose Tools | Templates
+and open the template in the editor.
+-->
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title></title>
+    </head>
+    <body>
+        
+        <form action="index.php">
+            
+            Customer Name : <input type="text" name="customerNameText"><br/>
+            
+            Account Number : <input type="text" name="accountNumberText">
+            
+            <input type="submit" name="createButton" value="Create">
+            
+            Amount : <input type="text" name="amountText"><br/>
+            
+             <input type="submit" name="depositButton" value="Deposit">
+             
+             <input type="submit" name="withdrawButton" value="Withdraw">
+              
+             <input type="submit" name="showReportButton" value="Show Report">
+            
+        </form>
+        
+        <?php
+        
+        require_once 'account.php';
+        
+        session_start();        
+       
+        if(isset($_GET['createButton']))
+        {
+            $account = new Account($_GET['customerNameText'], $_GET['accountNumberText']);
+            
+            echo 'Account Created';
+            
+            $_SESSION ['an_account'] = $account;
+        }        
+             
+        if(isset($_GET['depositButton']))
+        {
+            $_SESSION ['an_account']->deposit($_GET['amountText']);
+            echo "Diposited";
+        }
+        
+        if(isset($_GET['withdrawButton']))
+        {
+            $_SESSION ['an_account']->withdraw($_GET['amountText']);
+            echo "Withdrawn";
+        }
+        
+        if(isset($_GET['showReportButton']))
+        {
+            echo $_SESSION ['an_account']->get_account_name()."br/".' Your Balance is : '.$_SESSION ['an_account']->get_balance();
+        }
+        
+        
+        ?>
+    </body>
+</html>
